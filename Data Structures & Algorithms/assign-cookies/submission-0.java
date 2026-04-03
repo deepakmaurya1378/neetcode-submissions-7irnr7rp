7fc332608d8186e741f3fc1878200857e6d877c0
Deepak Maurya <138859870+deepakmaurya1378@users.noreply.github.com>
2026-04-03 13:13:56 +0530

    Add: assign-cookies - submission-0

---
+++ Data Structures & Algorithms/assign-cookies/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int n = g.length;
+        int m = s.length;
+        int l = 0; 
+        int r = 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        while (l < m && r < n) {
+            if (s[l] >= g[r]) {
+                r++; 
+            }
+            l++; 
+        }
+        
+        return r;
+    }
+}

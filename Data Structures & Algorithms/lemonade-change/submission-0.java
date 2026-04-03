70fd0329d03fd744091011c2acb65b00792e587d
Deepak Maurya <138859870+deepakmaurya1378@users.noreply.github.com>
2026-04-03 13:27:19 +0530

    Add: lemonade-change - submission-0

---
+++ Data Structures & Algorithms/lemonade-change/submission-0.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int ten = 0;
+        int five = 0;
+        for(int bill : bills){
+            if(bill == 5){
+                five++;
+            } 
+            else if(bill == 10){
+                if(five > 0){
+                    five--;
+                    ten++;
+                }
+                else{
+                     return false;
+                     }
+            }
+            else{
+                 if(five > 0 && ten > 0 ){
+                    five--;
+                    ten--;
+                 }
+                 else if(five > 2){
+                    five=five-3;
+                 }
+                else{ 
+                    return false; 
+                }
+            }
+        }
+        return true;
+    }
+}
